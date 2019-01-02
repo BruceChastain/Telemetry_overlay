4df93822f05a907ce7464310943090d7f3cbb8b0
Bruce Chastain <bruce.chastain@protonmail.ch>
2019-01-02 22:39:18 +0100

    Add files via upload

--- telemetry_video_overlay.py
+++ telemetry_video_overlay.py
@@ -1,15 +1,16 @@
 #!/usr/bin/python
-
 import sys
-#import mlt
 import gpxpy
 import ffmpeg
+from moviepy.editor import *
 
+#---------getting data from user
 vidinput = (sys.argv[1])
 vidoutput = (sys.argv[2])
 gpxinput = (sys.argv[3])
 telsync = (sys.argv[4]) 
 vidsync = (sys.argv[5])
+tellen = (sys.argv[6])
 
 
 
@@ -18,8 +19,23 @@ print (vidoutput)
 print (gpxinput)
 print (telsync)
 print (vidsync)
+print (tellen)
 
-gpx_file = open('example.gpx', 'r')
+
+
+#find the FPS !!!
+clip = VideoFileClip(vidinput)
+print(clip.fps)
+print(clip.duration)
+print(clip.is_playing(2))
+
+
+
+
+
+
+#---------reading gpx file and cauclating speed
+gpx_file = open(gpxinput, 'r')
 gpx = gpxpy.parse(gpx_file)
 
 for track in gpx.tracks:
@@ -30,24 +46,26 @@ for track in gpx.tracks:
             elif point_no > 0:
                 speed = (point.speed_between(segment.points[point_no - 1]))
                 kph_speed = speed * 3.6
-                print kph_speed
+                #print kph_speed
+
+
+
+#--------applying overlay and rendering video
+
+dur = 10
+
+video = VideoFileClip(vidinput)
 
 
 
 
-
-from moviepy.editor import *
-video = VideoFileClip('test_vid.avi')
-
-txt_clip = (TextClip("TESTY TESTY")
+txt_clip = (TextClip(str(kph_speed), fontsize=90,color='white')
             .set_position('center')
-            .set_duration(.1)            
+            .set_duration(dur)            
 )
 
 result = CompositeVideoClip([video, txt_clip])
-result.write_videofile("output.mp4")
+result.write_videofile(vidoutput)
 
-
-          
 
 
